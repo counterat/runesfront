cf110a2dce9e3038e99cb4d6022de4561f98b741
counterat <counteratios@gmail.com>
2024-06-02 01:41:14 +0300

    sasasascedfvdavcxc

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import ProfilePage from "./pages/ProfilePage";
 import FirstPage from "./pages/FirstPage";
-import { Routes, Route, json } from "react-router-dom";
+import { Routes, Route, json, Navigate  } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import GeneralPage from "./pages/GeneralPage";
 import { useRef } from "react";
@@ -18,9 +18,9 @@ function App() {
   user.current = useSelector(state=>state.user.user)
   const pathname = useRef();
   pathname.current = window.location.pathname;
-  alert(pathname.current)
+
   var invitCode = pathname.current.split('/')[1]
-  alert(invitCode)
+
   const [loading, setLoading] = useState(true);
   const dispatch = useDispatch()
   const isTest = true;
@@ -29,7 +29,7 @@ function App() {
     var WebApp = window.Telegram.WebApp; 
 
     WebApp.expand()
-   /*  var initdata = WebApp.initData  */
+  var initdata = WebApp.initData 
 
     socket.onmessage = function(event) {
       let data = (event.data)
@@ -47,16 +47,15 @@ function App() {
     }
                 
     if (isTest){
-    /*   authorise(initdata, invitCode).then(json=>{
+    authorise(initdata, invitCode).then(json=>{
         console.log(json)
        dispatch(setUser(json))
        alert( JSON.stringify(json))
     setLoading(false)
 
 
-      }) */
-      dispatch(setUser({'id': 7712793, 'sign': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ODgxNzA0ODkzLCJwYXNzd29yZCI6IjAxZTQ2YTg1LTFmM2MtNGNkZS04OTM0LTVjMDgzYjlmMGRlZiJ9.a5dfCs-_O_liSpPBihJUYlN8IrkiZfIUYH5Z0_XLhTk', 'telegram_id': 881704893, 'name': 'Gorilla', 'mana': 100, 'fehu_balance': '0', 'username': 'gorilla_bsrb', 'invited_by': 7712786, 'invitation_code': 166123632, 'invited_users': [], 'real_balance': '0', 'balances': {'1': [], '2': [], '3': [], '4': [], '5': []}, 'amount_of_money_withdrawed': 0.0, 'amount_of_money_topupped': 0.0, 'profit': '0', 'created_at': '2024-06-01 22:20:48.775993'}))
-      setLoading(false)
+      }) 
+    
     }
   }, []);
 
@@ -70,6 +69,7 @@ function App() {
           <Routes>
             <Route path="/" element={<GeneralPage />} />
             <Route path="/profile" element={<ProfilePage />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
           <ToastContainer />
         </>
