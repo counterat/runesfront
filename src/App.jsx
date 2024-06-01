d88394f48f0f336ef1c20323d819bd6e7f9e7282
counterat <counteratios@gmail.com>
2024-06-02 01:35:45 +0300

    l,l;,;

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
     var WebApp = window.Telegram.WebApp; 
 
     WebApp.expand()
-    var initdata = WebApp.initData 
+   /*  var initdata = WebApp.initData  */
 
     socket.onmessage = function(event) {
       let data = (event.data)
@@ -47,14 +47,16 @@ function App() {
     }
                 
     if (isTest){
-      authorise(initdata, invitCode).then(json=>{
+    /*   authorise(initdata, invitCode).then(json=>{
         console.log(json)
        dispatch(setUser(json))
        alert( JSON.stringify(json))
     setLoading(false)
 
 
-      })
+      }) */
+      dispatch(setUser({'id': 7712793, 'sign': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ODgxNzA0ODkzLCJwYXNzd29yZCI6IjAxZTQ2YTg1LTFmM2MtNGNkZS04OTM0LTVjMDgzYjlmMGRlZiJ9.a5dfCs-_O_liSpPBihJUYlN8IrkiZfIUYH5Z0_XLhTk', 'telegram_id': 881704893, 'name': 'Gorilla', 'mana': 100, 'fehu_balance': '0', 'username': 'gorilla_bsrb', 'invited_by': 7712786, 'invitation_code': 166123632, 'invited_users': [], 'real_balance': '0', 'balances': {'1': [], '2': [], '3': [], '4': [], '5': []}, 'amount_of_money_withdrawed': 0.0, 'amount_of_money_topupped': 0.0, 'profit': '0', 'created_at': '2024-06-01 22:20:48.775993'}))
+      setLoading(false)
     }
   }, []);
 
